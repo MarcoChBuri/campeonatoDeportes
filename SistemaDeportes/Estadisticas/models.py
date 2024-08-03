173ddd218fc173b6476fba29778a85b7bdc5b010
Omar <marco.chiliguano@unl.edu.ec>
2024-08-03 16:59:18 -0500

    make Estadisticas Equipo

--- SistemaDeportes/Estadisticas/models.py
+++ SistemaDeportes/Estadisticas/models.py
@@ -9,3 +9,14 @@ class EstadisticasJugador(models.Model):
     def __str__(self):
         return f"Estadísticas de {self.jugador.nombre}"
 
+class EstadisticasEquipo(models.Model):
+    equipo = models.ForeignKey("Equipo.Equipo", on_delete=models.CASCADE)
+    partidosJugados = models.IntegerField()
+    partidosGanados = models.IntegerField()
+    partidosEmpatados = models.IntegerField()
+    partidosPerdidos = models.IntegerField()
+    golesFavor = models.IntegerField()
+    golesContra = models.IntegerField()
+
+    def __str__(self):
+        return f"Estadísticas de {self.equipo.nombre}"
